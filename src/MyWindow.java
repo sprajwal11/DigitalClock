54d3fcf08d85cd797305605b3e12a4b4f94d81d6
Prajwal <sharmaprajwal11@gmail.com>
2022-02-08 01:24:06 +0530

    Clock Created, Formatting Left

--- src/MyWindow.java
+++ src/MyWindow.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
 
 public class MyWindow extends JFrame
 {
@@ -14,17 +17,37 @@ public class MyWindow extends JFrame
         super.setSize(400,400);
         super.setLocation(300,50);
 
+        this.creatGUI();
+        this.startClock();
+
         super.setVisible(true);
     }
 
     public void creatGUI(){
         heading=new JLabel("My Clock");
         clockLable=new JLabel("clock");
+
+
+
         heading.setFont(font);
         clockLable.setFont(font);
 
         this.setLayout(new GridLayout(2,1));
         this.add(heading);
+        this.add(clockLable);
+
+    }
+
+    public void startClock(){
+        Timer timer=new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String dateTime=new Date().toString();
+
+                clockLable.setText(dateTime);
+            }
+        });
+        timer.start();
 
     }
 }
